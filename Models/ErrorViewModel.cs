a633b82aaffbd894a7d6e6ccf32e3af719899b41
Dunghthe04 <tuandungyz09@gmail.com>
2026-03-17 21:39:43 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWD392_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
